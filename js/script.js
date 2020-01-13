ba989ed6b9a0608635c95c509274273a2530cd1d
Zara <zara.gma@gmail.com>
2020-01-12 22:49:54 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,88 @@
+$(window).on("load", function () {
+
+	$(".loader .inner").fadeOut(500, function () {
+		$(".loader").fadeOut(750);
+	});
+
+})
+
+
+$(document).ready(function () {
+
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		margin: 10,
+		nav: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 3
+			},
+			1000: {
+				items: 5
+			}
+		}
+	})
+	// Starts navigation bar
+	$("#navigation li a").click(function (e) {
+		e.preventDefault();
+
+		var targetElement = $(this).attr("href");
+		var targetPosition = $(targetElement).offset().top;
+		$("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+	});
+
+	const nav = $("#navigation");
+	const navTop = nav.offset().top;
+
+	$(window).on("scroll", stickyNavigation);
+
+	function stickyNavigation() {
+
+		var body = $("body");
+		if ($(window).scrollTop() >= navTop) {
+			body.css("padding-top", nav.outerHeight() + "px");
+			body.addClass("fixedNav");
+		}
+		else {
+			body.css("padding-top", 0);
+			body.removeClass("fixedNav");
+		}
+	}
+	// Ends navigation bar
+
+
+	// Starts projects
+	$("[data-fancybox]").fancybox();
+	$(".items").isotope({
+		filter: '*',
+		animationOptions: {
+			duration: 1500,
+			easing: 'linear',
+			queue: false
+		}
+	});
+
+	$("#filters a").click(function () {
+
+		$("#filters .current").removeClass("current");
+		$(this).addClass("current");
+
+		var selector = $(this).attr("data-filter");
+
+		$(".items").isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 1500,
+				easing: 'linear',
+				queue: false
+			}
+		});
+
+		return false;
+	});
+	// Ends Projects
+
+});
